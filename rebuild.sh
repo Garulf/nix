15f93bf98ec720bdb1a75c545e07d48624c03e28
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-02 16:28:41 -0500

    Update nixos-rebuild command to use --impure flag

--- rebuild.sh
+++ rebuild.sh
@@ -15,7 +15,7 @@ set -e
 echo "NixOS Rebuilding..."
 
 # Rebuild, output simplified errors, log trackebacks
-sudo nixos-rebuild switch --flake . &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
+sudo nixos-rebuild switch --flake . --impure &>nixos-switch.log || (cat nixos-switch.log | grep --color error && false)
 
 # Get current generation metadata
 current=$(nixos-rebuild list-generations | grep current)
