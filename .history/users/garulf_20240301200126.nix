3c85fc6fff5963ba296392a57de598e0a6a3d606
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-01 20:01:47 -0500

    More zsh

---
+++ .history/users/garulf_20240301200126.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }: 
+
+{
+  users.users.garulf = {
+    isNormalUser = true;
+    description = "garulf";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      firefox
+      discord
+    ];
+  };
+}
