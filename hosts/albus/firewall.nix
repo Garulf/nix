9daa3ab1dcdf9fc223c8baed8aaf75856a917fa9
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-07 01:11:17 -0500

    Add TCP and UDP ports for Sunshine admin and service

--- hosts/albus/firewall.nix
+++ hosts/albus/firewall.nix
@@ -3,7 +3,14 @@
 {
   networking.firewall.allowedTCPPorts = [ 
     6742 # OpenRGB
+    47990 # Sunshine admin
+    47984 # Sunshine
+    47989 # Sunshine
     47990 # Sunshine
+    48010 # Sunshine
+  ];
+  networking.firewall.allowedUDPPorts = [ 
+    { from = 47998; to = 48000; }
   ];
 
 }
