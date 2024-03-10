bd9fecd86236f8704ff438e2a573cced42aaef64
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-10 05:06:46 -0400

    Add gnome desktop manager for xrdp

--- hosts/common/rdp.nix
+++ hosts/common/rdp.nix
@@ -5,6 +5,7 @@
 
 
   services.xrdp.enable = true;
+  services.xrdp.desktopManager = "gnome";
   services.xrdp.defaultWindowManager = "gnome-remote-desktop";
   services.xrdp.openFirewall = true;
 }
