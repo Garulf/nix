691a4c970cd63fd900c6fca40d13d2c613900161
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-10 01:06:33 -0500

    141 current  2024-03-10 01:06:29  23.11.20240229.068d4db  6.1.79                          *

--- hosts/albus/default.nix
+++ hosts/albus/default.nix
@@ -139,6 +139,8 @@ in
     KERNEL=="uinput", SUBSYSTEM=="misc", OPTIONS+="static_node=uinput", TAG+="uaccess"
   '';
 
+  services.flatpak.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
