6d7fd391815c9ea2e58f9210aac97ddd325bc908
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-10 00:31:31 -0500

    140 current  2024-03-10 00:31:27  23.11.20240229.068d4db  6.1.79                          *

--- hosts/albus/default.nix
+++ hosts/albus/default.nix
@@ -124,7 +124,7 @@ in
   };
 
 
-  systemd.services.sunshine = {
+  systemd.user.services.sunshine = {
     description = "Sunshine is a Game stream host for Moonlight.";
     wantedBy = [ "graphical-session.target" ];
     serviceConfig = {
