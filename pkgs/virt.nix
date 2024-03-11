195d013525573817a9c148fde3cb996b938c4b84
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-10 23:08:02 -0400

    152 current  2024-03-10 23:07:58  23.11.20240229.068d4db  6.1.79                          *

--- pkgs/virt.nix
+++ pkgs/virt.nix
@@ -1,6 +1,13 @@
 { pkgs, lib, ...}:
 
 {
+  environment.systemPackages = with pkgs; [
+    qemu
+    libvirt
+    virt-viewer
+    virt-manager
+  ];
+
   virtualisation.libvirtd = {
     enable = true;
     qemu = {
@@ -9,7 +16,7 @@
       swtpm.enable = true;
       ovmf = {
         enable = true;
-        packages = [(pkgs.unstable.OVMF.override {
+        packages = [(pkgs.OVMF.override {
           secureBoot = true;
           tpmSupport = true;
         }).fd];
