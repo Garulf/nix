bc38ca8448b7e2648a78843975ee86f3b639c227
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-10 23:03:32 -0400

    Update virt.nix to enable libvirtd, qemu, and OVMF with secure boot and TPM support

--- pkgs/virt.nix
+++ pkgs/virt.nix
@@ -8,6 +8,19 @@
     virt-manager
   ];
 
-  virtualisation.libvirtd.enable = true;
-  programs.virt-manager.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [(pkgs.unstable.OVMF.override {
+          secureBoot = true;
+          tpmSupport = true;
+        }).fd];
+      };
+    };
+  };
 }
