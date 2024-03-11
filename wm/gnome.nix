b72378c80379a60c9a12795e4827fa10e20316e5
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-11 05:08:45 -0400

    Add i2c bool

--- wm/gnome.nix
+++ wm/gnome.nix
@@ -46,8 +46,5 @@
   };
 
   # Needed for ddcutil
-  boot.kernelModules = ["i2c-dev"];
-  services.udev.extraRules = ''
-        KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
-  '';
+  hardware.i2c.enable = true;
 }
