37a5aa32834a3f552978e0729e4bdd551094a845
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-13 04:17:19 -0400

    Add steam_autostart to environment.systemPackages

--- pkgs/steam.nix
+++ pkgs/steam.nix
@@ -1,17 +1,21 @@
 { pkgs, lib, ...}:
 
+let
+  steam_autostart = (
+    pkgs.makeAutostartItem { 
+      name = "steam"; 
+      package = my_steam;
+      after = "-nochatui -nofriendsui -silent"
+    }
+  );
+in
 {
-  systemd.user.services.steam = {
-    description = "Launches Steam on login.";
-    wantedBy = [ "graphical-session.target" ];
-    serviceConfig = {
-      Restart = "on-failure";
-      RestartSec = "5";
-      ExecStart = "${pkgs.steam}/bin/${pkgs.steam.pname} -nochatui -nofriendsui -silent";
-    };
-  };
+  environment.systemPackages = with pkgs; [
+    steam_autostart
+  ];
 
   programs.steam = {
+    enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall =true;
   };
