cc9decbbe851c5637a9c5df41cd616142041ef8e
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-13 03:35:53 -0400

    170 current  2024-03-13 03:32:34  23.11.20240229.068d4db  6.1.79                          *

--- pkgs/sunshine.nix
+++ pkgs/sunshine.nix
@@ -19,7 +19,7 @@
     serviceConfig = {
       Restart = "always";
       RestartSec = "5";
-      ExecStart = "${config.security.wrapperDir}/sunshine"
+      ExecStart = "${config.security.wrapperDir}/sunshine";
     };
   };
 
