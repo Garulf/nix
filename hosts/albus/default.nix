7ae8fe35f0221506bc93e0c9fb37836d7acc77a4
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-16 04:33:44 -0400

    Update polkit configuration and add systemd service
    This commit updates the polkit configuration in the `default.nix` file for the `albus` host. It adds a new polkit rule that allows users in the "power" group to perform certain actions. Additionally, a new systemd service `polkit-gnome-authentication-agent-1` is added to the `systemd` section. This service is configured to start with the graphical session and restart on failure.

--- hosts/albus/default.nix
+++ hosts/albus/default.nix
@@ -125,23 +125,39 @@ in
   security.polkit.enable = true;
 
   security.polkit.extraConfig = ''
-  polkit.addRule(function(action, subject) {
-    if (
-      subject.isInGroup("power")
-        && (
-          action.id == "org.freedesktop.login1.reboot" ||
-          action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
-          action.id == "org.freedesktop.login1.power-off" ||
-          action.id == "org.freedesktop.login1.power-off-multiple-sessions" ||
-          action.id == "org.freedesktop.login1.suspend" ||
-          action.id == "org.freedesktop.login1.suspend-multiple-sessions"
+    polkit.addRule(function(action, subject) {
+      if (
+        subject.isInGroup("power")
+          && (
+            action.id == "org.freedesktop.login1.reboot" ||
+            action.id == "org.freedesktop.login1.reboot-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.power-off" ||
+            action.id == "org.freedesktop.login1.power-off-multiple-sessions" ||
+            action.id == "org.freedesktop.login1.suspend" ||
+            action.id == "org.freedesktop.login1.suspend-multiple-sessions"
+          )
         )
-      )
-    {
-      return polkit.Result.YES;
-    }
-  })
-'';
+      {
+        return polkit.Result.YES;
+      }
+    })
+  '';
+
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = [ "graphical-session.target" ];
+      wants = [ "graphical-session.target" ];
+      after = [ "graphical-session.target" ];
+      serviceConfig = {
+          Type = "simple";
+          ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+          Restart = "on-failure";
+          RestartSec = 1;
+          TimeoutStopSec = 10;
+        };
+    };
+  };
 
   services.flatpak.enable = true;
 
