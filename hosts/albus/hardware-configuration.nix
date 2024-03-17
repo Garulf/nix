702789f573857767a65c323a50d3a688ce3d624a
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-17 03:02:38 -0400

    Add amd_iommu=on to boot.kernelModules

--- hosts/albus/hardware-configuration.nix
+++ hosts/albus/hardware-configuration.nix
@@ -12,7 +12,7 @@
 
   boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-amd" "amd_iommu=on" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
