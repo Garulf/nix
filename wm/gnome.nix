c09c6eef9344684ae09673cad45cc6c9ff02b25a
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-16 23:28:32 -0400

    Enable Wayland support in GDM

--- wm/gnome.nix
+++ wm/gnome.nix
@@ -5,7 +5,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-  # services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.displayManager.gdm.wayland = true;
   
   programs.dconf.profiles = {
       # TODO: Investigate customizing gdm greeter.
