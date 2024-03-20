1decd6ec38c8ae9d482be1e32e3aceb64fef17db
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-20 11:25:02 -0400

    210 current  2024-03-20 11:24:24  23.11.20240317.614b461  6.1.82                          *

--- hosts/albus/hardware-configuration.nix
+++ hosts/albus/hardware-configuration.nix
@@ -32,7 +32,7 @@
     };
 
   fileSystems."/mnt/Storage" =
-    { device = "/dev/disk/by-uuid/f5a9d26f-31b5-4434-a3b9-98b775ad4220";
+    { device = "/dev/disk/by-uuid/05737bff-9cb2-4940-946f-ca7c30d7c0f1";
       fsType = "ext4";
       options = ["nodev" "rw" "user" "exec" "nosuid" "nofail" "x-gvfs-show"];
       label = "Storage";
