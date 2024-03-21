b88c378d9eabfde369e10b9123d5642bd0cfcb67
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-21 08:24:19 -0400

    233 current  2024-03-21 08:07:55  23.11.20240317.614b461  6.1.82                          *

--- wm/gnome.nix
+++ wm/gnome.nix
@@ -4,7 +4,7 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = false;
+  services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.wayland = true;
   
   programs.dconf.profiles = {
