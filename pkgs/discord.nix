665296d99b229431a8c6426a78173d36bb60ecdb
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-22 13:09:14 -0400

    240 current  2024-03-22 13:09:08  23.11.20240317.614b461  6.1.82                          *

--- pkgs/discord.nix
+++ pkgs/discord.nix
@@ -1,19 +1,6 @@
 { pkgs, lib, ...}:
 
 {
-
-  desktopItems = [
-    (pkgs.makeDesktopItem rec {
-      name = "Sonic Robo Blast 2";
-      exec = "test";
-      icon = "test";
-      comment = "test";
-      desktopName = "test";
-      genericName = "test";
-      categories = [ "Game" ];
-    })
-  ];
-
   systemd.user.services.discord = {
     description = "Launches Discord on login.";
     wantedBy = [ "graphical-session.target" ];
