c6b3a5b3b8a3da6516b838851f7bfa0adf46aad5
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-24 23:39:14 -0400

    257 current  2024-03-24 22:35:03  23.11.20240317.614b461  6.1.82                          *

--- hosts/albus/default.nix
+++ hosts/albus/default.nix
@@ -2,7 +2,7 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ inputs, outputs, lib, config, pkgs, ... }:
 
 let
   monitorsXmlContent = builtins.readFile /home/garulf/.config/monitors.xml;
