45ca72b5f8ee658cd20665a6e0b7aa3c8aeac7e5
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-27 11:58:17 -0400

    271 current  2024-03-27 11:58:12  23.11.20240317.614b461  6.1.82                          *

--- pkgs/openrgb.nix
+++ pkgs/openrgb.nix
@@ -13,7 +13,7 @@
     serviceConfig = {
       Restart = "always";
       RestartSec = "5";
-      ExecStart = "${pkgs.unstable.openrgb-with-all-plugins}/bin/openrgb --noautoconnect --server --startminimized";
+      ExecStart = "${pkgs.unstable.openrgb-with-all-plugins}/bin/openrgb --noautoconnect --server";
     };
   };
 
