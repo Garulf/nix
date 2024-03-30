2057d76578857197c25f381eb972b13cd4d3efb8
Garulf <535299+Garulf@users.noreply.github.com>
2024-03-30 14:12:53 -0400

    Add mosh

--- hosts/albus/configuration.nix
+++ hosts/albus/configuration.nix
@@ -164,7 +164,8 @@ in
   virtualisation.waydroid.enable = true;
   virtualisation.docker.enable = true;
   services.flatpak.enable = false;
-
+  
+  programs.mosh.enable = true;
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
