345dc43d8a120a1f2e085723c57f696f72f16d6a
Garulf <535299+Garulf@users.noreply.github.com>
2024-04-05 09:14:49 -0400

    314 current  2024-04-05 09:14:42  23.11.20240317.614b461  6.1.82                          *

--- wm/x11/i3wm.nix
+++ wm/x11/i3wm.nix
@@ -20,4 +20,14 @@
       imagemagick
     ];
   };
+
+  environment.etc."systemd/system-sleep/openrgb.sh".source =
+    pkgs.writeShellScript "openrgb.sh" ''
+      if [ "$1" = "pre" ]; then
+        ${pkgs.openrgb}/bin/openrgb -c 000000
+      elif [ "$1" = "post" ]; then
+        ${pkgs.openrgb}/bin/openrgb -c ffffff
+      fi
+    '';
+
 }
