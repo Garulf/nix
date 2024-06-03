70e70c0fdd1584a4a70f4001069f7121cf20b3d8
Garulf <535299+Garulf@users.noreply.github.com>
2024-06-03 01:39:57 -0400

    345 current  2024-05-30 17:14:44  23.11.20240317.614b461  6.1.82                          *

--- pkgs/mpv.nix
+++ pkgs/mpv.nix
@@ -4,13 +4,22 @@
   nixpkgs.overlays = [
     (self: super: {
       mpv = super.mpv.override {
-        scripts = [ self.mpvScripts.mpris ];
+        scripts = [ 
+          self.mpvScripts.mpris
+          self.mpvScripts.mpv-osc-modern
+          self.mpvScripts.modernx
+          self.mpvScripts.sponsorblock
+          self.mpvScripts.quality-menu
+          self.mpvScripts.youtube-upnext
+          self.mpvScripts.mpv-notify-send
+        ];
       };
     })
   ];
 
   environment.systemPackages = with pkgs; [
     mpv
+    open-in-mpv
   ];
 
 }
