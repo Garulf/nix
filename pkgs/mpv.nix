afb92376a41da30520c503f9a079d5fa9d108ec9
Garulf <535299+Garulf@users.noreply.github.com>
2024-06-03 01:43:44 -0400

    345 current  2024-05-30 17:14:44  23.11.20240317.614b461  6.1.82                          *

--- pkgs/mpv.nix
+++ pkgs/mpv.nix
@@ -6,7 +6,6 @@
       mpv = super.mpv.override {
         scripts = [ 
           self.mpvScripts.mpris
-          self.mpvScripts.mpv-osc-modern
           self.mpvScripts.modernx
           self.mpvScripts.sponsorblock
           self.mpvScripts.quality-menu
