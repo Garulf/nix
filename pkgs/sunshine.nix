8d7c26e846ead61e21bb30ab59c3e81cdf7e657a
Garulf <535299+Garulf@users.noreply.github.com>
2024-06-13 20:21:59 -0400

    27 current  2024-06-13 20:21:56  24.05.20240606.9b5328b  6.6.32                          *

--- pkgs/sunshine.nix
+++ pkgs/sunshine.nix
@@ -1,7 +1,17 @@
 { config, pkgs, lib, ...}:
 
+let
+  sunshineOverride = pkgs.unstable.sunshine.override {
+    cudaSupport = true;
+    stdenv = pkgs.cudaPackages.backendStdenv;
+  };
+in
+
+with lib;
+
 {
   services.sunshine = {
+    package = sunshineOverride;
     enable = true;
     autoStart = true;
     capSysAdmin = true;
