5c86e05011681a96f8b0f0394769cad101945bc3
Garulf <535299+Garulf@users.noreply.github.com>
2024-06-14 03:24:09 -0400

    Add coding profile

---
+++ hosts/profiles/coding.nix
@@ -0,0 +1,20 @@
+{ inputs, outputs, lib, config, pkgs, ... }:
+
+{
+  imports =
+    [ 
+      ../../pkgs/neovim.nix
+    ];
+  
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.unstable-packages
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    vscode
+    python3Full
+    pipx
+  ];
+}
