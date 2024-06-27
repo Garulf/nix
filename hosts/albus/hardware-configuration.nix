a3303e210ed23b683abbfc1ec13fdd9e003af25b
Garulf <535299+Garulf@users.noreply.github.com>
2024-06-27 02:10:23 -0400

    62 current  2024-06-27 02:10:17  24.05.20240606.9b5328b  6.6.32                          *

--- hosts/albus/hardware-configuration.nix
+++ hosts/albus/hardware-configuration.nix
@@ -102,5 +102,5 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-  hardware.ckb-next.enable = false;
+  hardware.ckb-next.enable = true;
 }
