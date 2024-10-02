7fbefd7eb598d8d1b264b53fa82cc5b24af7b369
Garulf <535299+Garulf@users.noreply.github.com>
2024-10-01 23:26:15 -0400

    Update to unstable kernel

--- hosts/albus/configuration.nix
+++ hosts/albus/configuration.nix
@@ -50,7 +50,7 @@
   };
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  boot.kernelPackages = pkgs.linuxPackages_6_10;
+  boot.kernelPackages = unstable.linuxPackages_6_10;
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
